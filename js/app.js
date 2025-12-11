30920d06a1ba972d19c1abb93c37e51a81f90afd
Ben Manning <bmanning@me.com>
2025-12-11 14:17:47 -0500

    getting elements of questions to display on page

--- js/app.js
+++ js/app.js
@@ -4,44 +4,45 @@ let openSourceTriviaURLs = {
   twentyEasyQuestions: 'https://opentdb.com/api.php?amount=20&category=10&difficulty=easy&type=multiple',
 }
 
+// variables for gameplay
 let triviaQuestions;
+let questionNumber;
+let selections;
 
+// page elements
+const questionHeader = document.querySelector('.question');
 const button = document.querySelector('button');
 const playButton = document.querySelector('.play-button');
+const optionOneDisplay = document.querySelector('.option-one');
+const optionTwoDisplay = document.querySelector('.option-two');
+const optionThreeDisplay = document.querySelector('.option-three');
+const optionFourDisplay = document.querySelector('.option-four');
 
 button.addEventListener('click', async () => {
   const triviaAPIResponse = await fetch(openSourceTriviaURLs.twentyEasyQuestions);
   const jsonData = await triviaAPIResponse.json();
   triviaQuestions = jsonData.results;
+  console.log(triviaQuestions);
   button.style.visibility = 'hidden';
   playButton.style.visibility = 'visible';
 });
 
 playButton.addEventListener('click', () => {
-  console.log(triviaQuestions);
+  current = 0;
+  questionHeader.innerText = `${triviaQuestions[current].question}`;
+  selections = [...triviaQuestions[current].incorrect_answers, triviaQuestions[current].correct_answer];
+  optionOneDisplay.innerText = selections[0];
+  optionTwoDisplay.innerText = selections[1];
+  optionThreeDisplay.innerText = selections[2];
+  optionFourDisplay.innerText = selections[3];
 });
 
-// logic
 
-// questions and answers can be encapsulated in an object
 
-// example object
 
-// logic to display the question
-// const questionHeader = document.querySelector('.question');
-// questionHeader.innerText = exampleObj.question;
-
-// // logic to grab the options for display
-// const optionOneDisplay = document.querySelector('.option-one');
-// const optionTwoDisplay = document.querySelector('.option-two');
-// const optionThreeDisplay = document.querySelector('.option-three');
-// const optionFourDisplay = document.querySelector('.option-four');
 
 // // logic to display the options onto the page
-// optionOneDisplay.innerText = exampleObj['option-one'];
-// optionTwoDisplay.innerText = exampleObj['option-two'];
-// optionThreeDisplay.innerText = exampleObj['option-three'];
-// optionFourDisplay.innerText = exampleObj['option-four'];
+
 
 // const clickableButtons = document.querySelectorAll('.selection-button');
 
